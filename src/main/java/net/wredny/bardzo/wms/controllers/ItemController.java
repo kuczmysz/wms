cf074c7a8e5eb57ed2181419861965d054a73adc
kuczmysz <bardzo@wredny.net>
2021-03-15 02:53:40 +0100

    - added docker-compose.yml and Dockerfile

--- src/main/java/net/wredny/bardzo/wms/controllers/ItemController.java
+++ src/main/java/net/wredny/bardzo/wms/controllers/ItemController.java
@@ -2,6 +2,7 @@ package net.wredny.bardzo.wms.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,29 +25,34 @@ class ItemController {
 	@Autowired
 	private ItemReactiveRepository repository;
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/{id}")
 	public Mono<Item> get(@PathVariable String id) {
 		return repository.findById(id);
 	}
 
-	@GetMapping(path = "/all", produces = MediaType.APPLICATION_NDJSON_VALUE)
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping(path = "/all")//, produces = MediaType.APPLICATION_NDJSON_VALUE)
 	public Flux<Item> getAll() {
 		Flux<Item> items = repository.findAll();
 		return items;
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping
 	public Mono<Item> save(@RequestBody Item item) {
 		Mono<Item> savedItem = repository.save(item);
 		return savedItem;
 	}
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping("/{id}")
 	public Mono<Void> delete(@PathVariable String id) {
 		return repository.deleteById(id);
 	}
 
-	@PostMapping("/all")
+	@CrossOrigin(origins = "http://localhost:4200")
+	@DeleteMapping("/all")
 	public Flux<Item> saveAll(@RequestBody Flux<Item> items) {
 		return repository.saveAll(items);
 	}
